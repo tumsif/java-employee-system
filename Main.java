4a6b34a696b5f7dd9e42f49401fc41c07d4023c9
Tumsifu Lema <tom455976@gmail.com>
2024-05-27 14:07:49 +0300

    added employee manager class

--- Main.java
+++ Main.java
@@ -56,6 +56,56 @@ class Employee {
     }
 }
 
+class EmployeeManager {
+
+    private ArrayList<Employee> employees = new ArrayList<Employee>();
+
+    void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    void removeEmployee(int index) {
+        // check if the user has entered invalid index
+        if (index >= 0 && index < employees.size()) {
+            employees.remove(index);
+        } else {
+            System.out.println("No employee to remove");
+        }
+
+    }
+
+    ArrayList<Employee> getAllEmployee() {
+        return employees;
+    }
+
+    Employee getEmployee(int index) {
+        return getAllEmployee().get(index);
+    }
+
+    void displayAllEmployees() {
+        // displaying the list of employee
+        // check if there are employees
+        System.out.println();
+        if (getAllEmployee().isEmpty()) {
+            System.out.println("There are " + getAllEmployee().size() + " employees");
+            System.out.println("We have nothing to show");
+            System.out.println();
+        } else {
+            System.out.println("There are " + getAllEmployee().size() + " employees");
+            System.out.println("The following is the list of employees");
+            System.out.println("No | ID | Name | Department | Salary ");
+            for (int i = 0; i < getAllEmployee().size(); i++) {
+                System.out.println((i + 1) + "   " + getEmployee(i).getId() + "    "
+                        + getEmployee(i).getName()
+                        + "      " + getEmployee(i).getDepartment() + "      "
+                        + getEmployee(i).getSalary());
+
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         // Indicate the user if the application has started
@@ -72,12 +122,16 @@ public class Main {
 
         // create an array list for storing Employee object this is to store the
         // employee details
-        ArrayList<Employee> employees = new ArrayList<Employee>();
+        // ArrayList<Employee> employees = new ArrayList<Employee>();
         // scanner object for user input
         Scanner userinput = new Scanner(System.in);
 
         // an empty string that holds command for the application
         String command = "";
+
+        // calling the Employee manager class
+        EmployeeManager employeemanager = new EmployeeManager();
+
         while ("quit".equals(command) == false) {
             // listen for user command
             System.out.print("Enter your command: ");
@@ -87,26 +141,26 @@ public class Main {
             if ("display".equals(command) == true) {
                 // displaying the list of employee
                 // check if there are employees
-                System.out.println();
-                if (employees.isEmpty()) {
-                    System.out.println("There are " + employees.size() + " employees");
-                    System.out.println("We have nothing to show");
-                    System.out.println();
-                } else {
-                    System.out.println("There are " + employees.size() + " employees");
-                    System.out.println("The following is the list of employees");
-                    System.out.println("No | ID | Name | Department | Salary ");
-                    for (int i = 0; i < employees.size(); i++) {
-                        System.out
-                                .println(
-                                        (i + 1) + "   " + employees.get(i).getId() + "    " + employees.get(i).getName()
-                                                + "      " + employees.get(i).getDepartment() + "      "
-                                                + employees.get(i).getSalary());
+                // System.out.println();
+                // if (employees.isEmpty()) {
+                // System.out.println("There are " + employees.size() + " employees");
+                // System.out.println("We have nothing to show");
+                // System.out.println();
+                // } else {
+                // System.out.println("There are " + employees.size() + " employees");
+                // System.out.println("The following is the list of employees");
+                // System.out.println("No | ID | Name | Department | Salary ");
+                // for (int i = 0; i < employees.size(); i++) {
+                // System.out
+                // .println(
+                // (i + 1) + " " + employees.get(i).getId() + " " + employees.get(i).getName()
+                // + " " + employees.get(i).getDepartment() + " "
+                // + employees.get(i).getSalary());
 
-                    }
-                    System.out.println();
-                }
-
+                // }
+                // System.out.println();
+                // }
+                employeemanager.displayAllEmployees();
             } else if ("add".equals(command) == true) {
                 // adding employees to the array
                 System.out.println("Adding a new employee");
@@ -128,45 +182,47 @@ public class Main {
                 String emp_department = userinput.nextLine();
                 // no need to clear buffer
                 Employee employee = new Employee(emp_id, emp_name, emp_department, emp_salary);
-                employees.add(employee);
+                employeemanager.addEmployee(employee);
+                // employees.add(employee);
                 System.out.println("Successfully added a new employee");
 
             } else if ("remove".equals(command) == true) {
                 // command to remove employees
                 System.out.println();
                 System.out.println("Removing an employee!");
-                if (employees.isEmpty()) {
-                    System.out.println("There are " + employees.size() + " employees");
-                    System.out.println("Add employee before removing");
-                    System.out.println();
-                } else {
-                    System.out.println("There are " + employees.size() + " employees");
-                    System.out.println("The following is the list of employees");
-                    System.out.println("No | ID | Name | Department | Salary ");
-                    for (int i = 0; i < employees.size(); i++) {
-                        System.out
-                                .println(
-                                        (i + 1) + "   " + employees.get(i).getId() + "    " + employees.get(i).getName()
-                                                + "      " + employees.get(i).getDepartment() + "      "
-                                                + employees.get(i).getSalary());
-                        System.out.println();
-                    }
-                    System.out.println();
-                    System.out.print("Select Employee by no to remove (number only) ");
-                    int removeindex = userinput.nextInt();
-                    userinput.nextLine();
-                    // subtract 1 to ensure the indexing is followed
-                    removeindex -= 1;
-                    employees.remove(removeindex);
-                    System.out.println("Successfully removed the employee");
+                // if (employees.isEmpty()) {
+                // System.out.println("There are " + employees.size() + " employees");
+                // System.out.println("Add employee before removing");
+                // System.out.println();
+                // } else {
+                // System.out.println("There are " + employees.size() + " employees");
+                // System.out.println("The following is the list of employees");
+                // System.out.println("No | ID | Name | Department | Salary ");
+                // for (int i = 0; i < employees.size(); i++) {
+                // System.out
+                // .println(
+                // (i + 1) + " " + employees.get(i).getId() + " " + employees.get(i).getName()
+                // + " " + employees.get(i).getDepartment() + " "
+                // + employees.get(i).getSalary());
+                // System.out.println();
+                // }
+                employeemanager.displayAllEmployees();
+                System.out.println();
+                System.out.print("Select Employee by no to remove (number only) ");
+                int removeindex = userinput.nextInt();
+                userinput.nextLine();
+                // subtract 1 to ensure the indexing is followed
+                removeindex -= 1;
+                // employees.remove(removeindex);
+                employeemanager.removeEmployee(removeindex);
 
-                }
+                System.out.println("Successfully removed the employee number " + (removeindex + 1));
             } else if ("quit".equals(command) == true) {
+                System.out.println();
                 System.out.println("Exiting the application...");
             } else {
                 System.out.println("Command '" + command + "' not found.");
             }
-
         }
         System.out.println("You have successfully exit the application");
         // closing the scanner object
